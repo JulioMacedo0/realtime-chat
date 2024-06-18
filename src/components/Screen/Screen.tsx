bc5bbd25ef7a1cf47eb357bbbf9b7defc21b198c
Julio Macedo <juliocesar1076@gmail.com>
2024-06-18 11:13:07 -0300

    feat: create screen component

---
+++ src/components/Screen/Screen.tsx
@@ -0,0 +1,38 @@
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  useColorScheme,
+} from "react-native";
+import { ThemedView } from "../ThemedView";
+import { Colors } from "@/constants/Colors";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export function Screen({ children }: Props) {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemedView
+      style={[
+        styles.container,
+        { backgroundColor: Colors[colorScheme ?? "light"].background },
+      ]}
+    >
+      {children}
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  flex: {
+    flex: 1,
+  },
+});
