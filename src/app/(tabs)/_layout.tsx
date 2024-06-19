49e1aeb7ba78bf145b00dd9596ae0687c1de0db6
Julio Macedo <juliocesar1076@gmail.com>
2024-06-18 22:56:17 -0300

    feat: add gesture root view

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -4,61 +4,64 @@ import React from "react";
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarStyle: {
-          backgroundColor: Colors[colorScheme ?? "light"].tabBarBackground,
-        },
-        headerStyle: {
-          backgroundColor: Colors[colorScheme ?? "light"].headerBackground,
-        },
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: true,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Real time chat",
-          headerTitleAlign: "center",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "home" : "home-outline"}
-              color={color}
-            />
-          ),
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: Colors[colorScheme ?? "light"].tabBarBackground,
+          },
+          headerStyle: {
+            backgroundColor: Colors[colorScheme ?? "light"].headerBackground,
+          },
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+          headerShown: true,
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: "Explore",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "code-slash" : "code-slash-outline"}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="todo"
-        options={{
-          title: "todo",
-          headerTitleAlign: "center",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "book" : "book-outline"}
-              color={color}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Real time chat",
+            headerTitleAlign: "center",
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon
+                name={focused ? "home" : "home-outline"}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            title: "Explore",
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon
+                name={focused ? "code-slash" : "code-slash-outline"}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="todo"
+          options={{
+            title: "todo",
+            headerTitleAlign: "center",
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon
+                name={focused ? "book" : "book-outline"}
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </GestureHandlerRootView>
   );
 }
