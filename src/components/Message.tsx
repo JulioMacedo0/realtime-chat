078137f0b4b08cd7cce71c24d929a902065b789a
Julio Macedo <juliocesar1076@gmail.com>
2024-06-26 15:03:02 -0300

    feat: add component

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { TMessage } from "@/app/(tabs)";
+import { contentType, TMessage } from "@/app/(tabs)";
 import { StyleSheet, useWindowDimensions, View } from "react-native";
 import { ThemedView } from "./ThemedView";
 import { ThemedText } from "./ThemedText";
 import { Image } from "expo-image";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { Colors } from "@/constants/Colors";
-import { useMessageStore } from "@/store/messageStore";
-
-const blurhash =
-  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
+import { useMessages } from "@/store/messageStore";
+import { format } from "date-fns";
+import { PorfilePicture } from "./ProfilePicture";
 
 type Props = {
   message: TMessage;
@@ -20,8 +19,10 @@ type Props = {
 export function Message({ message, userId, index }: Props) {
   const colorScheme = useColorScheme();
 
-  const messages = useMessageStore((state) => state.messages);
+  const messages = useMessages();
+
   const { width } = useWindowDimensions();
+
   const isUserMessage = message.user.id === userId;
   const backgroundColor = isUserMessage
     ? "#0a7ea4"
@@ -50,18 +51,9 @@ export function Message({ message, userId, index }: Props) {
         }}
       >
         {!isUserMessage && (
-          <Image
+          <PorfilePicture
+            isSeq={isSeq}
             source="https://avatars.githubusercontent.com/u/57598810?v=4"
-            style={{
-              opacity: isSeq ? 0 : 1,
-              width: 30,
-              height: 30,
-              borderRadius: 999,
-              marginRight: 10,
-            }}
-            placeholder={{ blurhash }}
-            contentFit="cover"
-            transition={1000}
           />
         )}
         <View
@@ -97,23 +89,27 @@ export function Message({ message, userId, index }: Props) {
               justifyContent: "space-between",
             }}
           >
-            <ThemedText
-              style={{
-                color: Colors[colorScheme ?? "light"].text,
-              }}
-            >
-              {message.content.message}
-            </ThemedText>
-            <ThemedText
-              style={{
-                color: "#ccc",
-                fontSize: 13,
-                alignSelf: "flex-end",
-                marginLeft: 10,
-              }}
-            >
-              15:30
-            </ThemedText>
+            {message.content.type == contentType.message && (
+              <>
+                <ThemedText
+                  style={{
+                    color: Colors[colorScheme ?? "light"].text,
+                  }}
+                >
+                  {message.content.message}
+                </ThemedText>
+                <ThemedText
+                  style={{
+                    color: "#ccc",
+                    fontSize: 13,
+                    alignSelf: "flex-end",
+                    marginLeft: 10,
+                  }}
+                >
+                  {format(message.content.date, "HH:mm")}
+                </ThemedText>
+              </>
+            )}
           </View>
         </View>
       </ThemedView>
