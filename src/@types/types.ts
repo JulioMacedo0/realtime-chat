1cd78fd53bbf233b3e8a273f485e50cecee86505
Julio Macedo <juliocesar1076@gmail.com>
2024-07-08 16:43:51 -0300

    feat: add types

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -1,9 +1,54 @@
-import { IFormInput } from "@/app/(tabs)";
-
-export type TMessage = IFormInput;
-
 export enum contentType {
   message = "message",
   photo = "photo",
   video = "video",
 }
+
+type contentBase = {
+  id: string;
+  date: string;
+};
+
+export type contentMessage = contentBase & {
+  type: contentType.message;
+  message: string;
+};
+
+export type contentPhoto = {
+  type: contentType.photo;
+  message: string;
+  url: string;
+  previewUrl: string;
+  meta: {
+    localUri: string;
+  } & contentBase;
+};
+
+export type contentVideo = {
+  type: contentType.video;
+  message: string;
+  url: string;
+  previewUrl: string;
+  meta: {
+    localUri: string;
+  };
+} & contentBase;
+
+export type ContentPayload = {
+  content: contentMessage | contentPhoto | contentVideo;
+  user: {
+    id: string;
+  };
+};
+
+export type ContentMessagePayload = {
+  content: contentMessage;
+} & ContentPayload;
+
+export type ContentPhotoPayload = {
+  content: contentPhoto;
+} & ContentPayload;
+
+export type ContentVideoPayload = {
+  content: contentVideo;
+} & ContentPayload;
