d8404dc52f539da9255d263d01f2005a51ca3c8b
Julio Macedo <juliocesar1076@gmail.com>
2024-07-08 16:45:52 -0300

    feat: add user id constant for test

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./Tsupabase";
-
+import * as Crypto from "expo-crypto";
 if (!process.env.EXPO_PUBLIC_SUPABASE_KEY) {
   throw new Error("env.EXPO_PUBLIC_SUPABASE_KEY is required");
 }
@@ -15,3 +15,4 @@ if (!process.env.EXPO_PUBLIC_SUPABASE_CHAT_BUCKET) {
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+export const USER_ID = Crypto.randomUUID();
