2f91ad694841b06233cff2da521b17e8a8de3019
Julio Macedo <juliocesar1076@gmail.com>
2024-07-10 16:33:24 -0300

    feat: add  props

--- src/components/InputChat.tsx
+++ src/components/InputChat.tsx
@@ -1,104 +1,72 @@
-import { ContentPayload } from "@/@types/types";
-
-import { IconApp } from "@/components";
-import { Camera } from "@/components/Camera";
 import { Colors } from "@/constants/Colors";
-import { USER_ID } from "@/supabase/supabase";
-import { Controller, useForm } from "react-hook-form";
+import React from "react";
 import {
   TextInput,
-  TouchableOpacity,
   View,
   useColorScheme,
   StyleSheet,
+  ViewStyle,
+  StyleProp,
+  TextStyle,
+  type TextInputProps,
 } from "react-native";
 
-type Props = {};
-export const Inputchat = () => {
-  const {
-    control,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<ContentPayload>({
-    defaultValues: {
-      content: {
-        message: "",
-      },
-      user: {
-        id: USER_ID,
-      },
-    },
-  });
-
+type Props = TextInputProps & {
+  containerInputStyle?: StyleProp<ViewStyle>;
+  TextInputStyle?: StyleProp<TextStyle>;
+  prefix?: React.JSX.Element;
+  suffixs?: React.JSX.Element[];
+};
+export const Inputchat = ({
+  containerInputStyle,
+  TextInputStyle,
+  prefix,
+  suffixs,
+  ...rest
+}: Props) => {
   const colorScheme = useColorScheme();
   return (
     <View
-      style={{
-        flex: 1,
-        flexDirection: "row",
-        alignItems: "center",
-        backgroundColor: Colors[colorScheme ?? "light"].headerBackground,
-        borderRadius: 28,
-        borderColor: "#ccc",
-        borderWidth: 1,
-        paddingHorizontal: 12,
-        gap: 6,
-      }}
+      style={[
+        {
+          backgroundColor: Colors[colorScheme ?? "light"].headerBackground,
+        },
+        styles.inputContainer,
+        containerInputStyle,
+      ]}
     >
-      <TouchableOpacity activeOpacity={0.4}>
-        <IconApp lib="FontAwesome6" name="face-laugh" color="#ccc" />
-      </TouchableOpacity>
+      {prefix}
 
-      <Controller
-        control={control}
-        rules={{}}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            placeholderTextColor="#ccc"
-            style={[
-              styles.input,
-              {
-                color: Colors[colorScheme ?? "light"].text,
-              },
-            ]}
-            placeholder="Message"
-            onBlur={onBlur}
-            onChangeText={(text) => {
-              onChange(text);
-              if (text.length == 0) {
-              } else if (text.length >= 1) {
-              }
-            }}
-            value={value}
-          />
-        )}
-        name="content.message"
+      <TextInput
+        placeholderTextColor="#ccc"
+        placeholder="Message"
+        style={[
+          styles.input,
+          {
+            color: Colors[colorScheme ?? "light"].text,
+          },
+          TextInputStyle,
+        ]}
+        {...rest}
       />
-
-      <TouchableOpacity activeOpacity={0.4}>
-        <IconApp lib="MaterialCommunityIcons" name="paperclip" color="#ccc" />
-      </TouchableOpacity>
-
-      <Camera />
+      {suffixs &&
+        suffixs.map((suffix, index) => (
+          <React.Fragment key={index}>{suffix}</React.Fragment>
+        ))}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  title: {
-    marginBottom: 16,
-    textAlign: "center",
-  },
-  messageList: {
-    flex: 1,
-    marginBottom: 16,
-  },
-
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     alignItems: "center",
-    gap: 4,
+    borderRadius: 28,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    paddingHorizontal: 12,
+    gap: 6,
   },
   input: {
     flex: 1,
