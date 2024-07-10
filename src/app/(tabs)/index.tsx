85dfb74b129f1ad9ff782249f3d014f4028ff1f4
Julio Macedo <juliocesar1076@gmail.com>
2024-07-10 10:23:06 -0300

    feat: separate components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -44,6 +44,8 @@ import {
   contentType,
   contentVideo,
 } from "@/@types/types";
+import { Inputchat } from "@/components/InputChat";
+import { SendAudioButton } from "@/components/SendAudioButton";
 
 type BroadcastPayload = {
   event: string;
@@ -51,87 +53,14 @@ type BroadcastPayload = {
   type: string;
 };
 
-type ContextType = {
-  startX: number;
-  startY: number;
-};
-
-type Clamp = {
-  val: number;
-  min: number;
-  max: number;
-};
-
-type Gesture = GestureUpdateEvent<PanGestureHandlerEventPayload>;
-
 export default function HomeScreen() {
-  function clamp({ val, min, max }: Clamp) {
-    return Math.min(Math.max(val, min), max);
-  }
-
   const animatedRef = useAnimatedRef<Animated.FlatList<ContentPayload>>();
-
-  const {
-    control,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<ContentPayload>({
-    defaultValues: {
-      content: {
-        message: "",
-      },
-      user: {
-        id: USER_ID,
-      },
-    },
-  });
 
   const messages = useMessages();
 
   const { addMessage } = useMessagesActions();
 
-  const [hasMessage, setHasmessage] = useState(false);
   const { height } = useWindowDimensions();
-  const colorScheme = useColorScheme();
-
-  const scale = useSharedValue(1);
-
-  const translationX = useSharedValue(0);
-  const translationY = useSharedValue(0);
-  const prevTranslationX = useSharedValue(0);
-  const prevTranslationY = useSharedValue(0);
-
-  const MAX_TRANSLATE_X = 200;
-  const MAX_TRANSLATE_Y = 115;
-
-  const animatedPressableStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ scale: scale.value }],
-    };
-  });
-  const animatedViewStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: translationX.value },
-        { translateY: translationY.value },
-      ],
-    };
-  });
-
-  const onPressIn = () => {
-    scale.value = withSpring(2, {
-      damping: 8,
-      stiffness: 150,
-    });
-  };
-
-  const onPressOut = () => {
-    scale.value = withSpring(1, {
-      damping: 8,
-      stiffness: 150,
-    });
-  };
 
   useEffect(() => {
     const channel = supabase
@@ -163,101 +92,6 @@ export default function HomeScreen() {
     Alert.alert("Limite máximo atingido!");
   };
 
-  let initialDirection: null | "x" | "y" = null;
-  const TOLERANCE = 10;
-  const panGesture = Gesture.Pan()
-    .enabled(!hasMessage)
-    .minDistance(1)
-    .onBegin(() => {
-      scale.value = withSpring(2.2, {
-        damping: 8,
-        stiffness: 150,
-      });
-    })
-    .onStart((event) => {
-      prevTranslationX.value = translationX.value;
-      prevTranslationY.value = translationY.value;
-    })
-    .onUpdate((event) => {
-      const maxTranslateX = MAX_TRANSLATE_X;
-      const maxTranslateY = MAX_TRANSLATE_Y;
-
-      let nextTranslateX = event.translationX;
-      let nextTranslateY = event.translationY;
-
-      if (nextTranslateX >= 0) {
-        initialDirection = null;
-      }
-
-      if (nextTranslateY >= 0) {
-        initialDirection = null;
-      }
-
-      if (initialDirection === null) {
-        if (
-          Math.abs(nextTranslateX) > TOLERANCE ||
-          Math.abs(nextTranslateY) > TOLERANCE
-        ) {
-          if (Math.abs(nextTranslateX) > Math.abs(nextTranslateY)) {
-            initialDirection = "x";
-          } else {
-            initialDirection = "y";
-          }
-        }
-      }
-
-      if (nextTranslateX > 0) {
-        nextTranslateX = 0;
-      }
-      if (nextTranslateY > 0) {
-        nextTranslateY = 0;
-      }
-
-      if (initialDirection === "x") {
-        nextTranslateY = translationY.value;
-      } else {
-        nextTranslateX = translationX.value;
-      }
-
-      if (
-        Math.abs(nextTranslateX) >= maxTranslateX ||
-        Math.abs(nextTranslateY) >= maxTranslateY
-      ) {
-        // runOnJS(onMaxReached)();
-      } else {
-        translationX.value = clamp({
-          val: nextTranslateX,
-          min: -maxTranslateX,
-          max: maxTranslateX,
-        });
-
-        translationY.value = clamp({
-          val: nextTranslateY,
-          min: -maxTranslateY,
-          max: maxTranslateY,
-        });
-      }
-    })
-    .onEnd(() => {
-      translationX.value = withSpring(0);
-      translationY.value = withSpring(0);
-      scale.value = withSpring(1, {
-        damping: 8,
-        stiffness: 150,
-      });
-      initialDirection = null;
-    })
-    .onTouchesUp(() => {
-      translationX.value = withSpring(0);
-      translationY.value = withSpring(0);
-      scale.value = withSpring(1, {
-        damping: 8,
-        stiffness: 150,
-      });
-      initialDirection = null;
-    })
-    .runOnJS(true);
-
   const renderItem = ({
     item,
     index,
@@ -273,8 +107,6 @@ export default function HomeScreen() {
       }}
     />
   );
-
-  const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
   const onSubmit: SubmitHandler<ContentPayload> = (data) => {
     console.log(data);
@@ -292,17 +124,8 @@ export default function HomeScreen() {
       user: { ...data.user },
     };
     sendMessage(payload);
-    reset();
-    setHasmessage(false);
   };
 
-  const handleButton = () => {
-    if (hasMessage) {
-      handleSubmit(onSubmit)();
-    } else {
-      console.log("send audio");
-    }
-  };
   return (
     <Screen>
       <Animated.FlatList
@@ -315,87 +138,8 @@ export default function HomeScreen() {
       />
 
       <View style={styles.inputContainer}>
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            alignItems: "center",
-            backgroundColor: Colors[colorScheme ?? "light"].headerBackground,
-            borderRadius: 28,
-            borderColor: "#ccc",
-            borderWidth: 1,
-            paddingHorizontal: 12,
-            gap: 6,
-          }}
-        >
-          <TouchableOpacity activeOpacity={0.4}>
-            <IconApp lib="FontAwesome6" name="face-laugh" color="#ccc" />
-          </TouchableOpacity>
-
-          <Controller
-            control={control}
-            rules={{}}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                placeholderTextColor="#ccc"
-                style={[
-                  styles.input,
-                  {
-                    color: Colors[colorScheme ?? "light"].text,
-                  },
-                ]}
-                placeholder="Message"
-                onBlur={onBlur}
-                onChangeText={(text) => {
-                  onChange(text);
-                  if (text.length == 0) {
-                    setHasmessage(false);
-                  } else if (text.length >= 1) {
-                    setHasmessage(true);
-                  }
-                }}
-                value={value}
-              />
-            )}
-            name="content.message"
-          />
-
-          <TouchableOpacity activeOpacity={0.4}>
-            <IconApp
-              lib="MaterialCommunityIcons"
-              name="paperclip"
-              color="#ccc"
-            />
-          </TouchableOpacity>
-
-          <Camera />
-        </View>
-
-        <GestureDetector gesture={panGesture}>
-          <Animated.View style={animatedViewStyle}>
-            <AnimatedPressable
-              hitSlop={20}
-              onPress={handleButton}
-              // onPressOut={onPressOut}
-              style={[
-                {
-                  backgroundColor: "#00af00",
-                  padding: 10,
-                  borderRadius: 999,
-                  justifyContent: "center",
-                  alignItems: "center",
-                },
-                animatedPressableStyle,
-              ]}
-            >
-              <IconApp
-                lib="Ionicons"
-                name={hasMessage ? "send-sharp" : "mic"}
-                color="#000"
-              />
-            </AnimatedPressable>
-          </Animated.View>
-        </GestureDetector>
+        <Inputchat />
+        <SendAudioButton />
       </View>
     </Screen>
   );
