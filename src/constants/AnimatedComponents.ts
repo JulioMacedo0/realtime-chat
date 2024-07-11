f58770cd3d464022cd15277f74cc42cd8866925d
Julio Macedo <juliocesar1076@gmail.com>
2024-07-11 10:34:47 -0300

    feat: create animated activity indicator

--- src/constants/AnimatedComponents.ts
+++ src/constants/AnimatedComponents.ts
@@ -1,4 +1,7 @@
-import { Pressable } from "react-native";
+import { IconApp } from "@/components";
+import { Pressable, ActivityIndicator } from "react-native";
 import Animated from "react-native-reanimated";
 
 export const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+export const AnimatedActivityIndicator =
+  Animated.createAnimatedComponent(ActivityIndicator);
