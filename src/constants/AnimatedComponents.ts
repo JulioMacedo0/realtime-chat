0756d24440e644b90ca0ce13885cd8da787b51e8
Julio Macedo <juliocesar1076@gmail.com>
2024-07-11 11:15:24 -0300

    feat: create animated component

--- src/constants/AnimatedComponents.ts
+++ src/constants/AnimatedComponents.ts
@@ -1,7 +1,9 @@
 import { IconApp } from "@/components";
-import { Pressable, ActivityIndicator } from "react-native";
+import { Pressable, ActivityIndicator, TouchableOpacity } from "react-native";
 import Animated from "react-native-reanimated";
 
 export const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 export const AnimatedActivityIndicator =
   Animated.createAnimatedComponent(ActivityIndicator);
+export const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity);
