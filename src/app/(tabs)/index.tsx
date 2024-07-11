2bf332d1d0270640380f6b4ad1a2fa9c78886866
Julio Macedo <juliocesar1076@gmail.com>
2024-07-11 10:34:26 -0300

    feat: add animated list

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,21 +1,16 @@
 import React, { useEffect } from "react";
-import {
-  StyleSheet,
-  View,
-  ListRenderItemInfo,
-  useWindowDimensions,
-} from "react-native";
+import { StyleSheet } from "react-native";
 import Animated, { useAnimatedRef } from "react-native-reanimated";
 
 import { USER_ID, supabase } from "@/supabase/supabase";
 
 import { Screen } from "@/components";
 
-import { Message } from "@/components/Message";
-import { useMessages, useMessagesActions } from "@/store/messageStore";
+import { useMessagesActions } from "@/store/messageStore";
 import { ContentPayload } from "@/@types/types";
 
 import { BottomChatInput } from "@/components/BottomChatInput";
+import { MessageList } from "@/components/MessagesList";
 
 type BroadcastPayload = {
   event: string;
@@ -24,13 +19,9 @@ type BroadcastPayload = {
 };
 
 export default function HomeScreen() {
+  console.log("HomeScreen");
   const animatedRef = useAnimatedRef<Animated.FlatList<ContentPayload>>();
-
-  const messages = useMessages();
-
   const { addMessage } = useMessagesActions();
-
-  const { height } = useWindowDimensions();
 
   useEffect(() => {
     const channel = supabase
@@ -38,8 +29,8 @@ export default function HomeScreen() {
       .on("broadcast", { event: `message` }, (payload: BroadcastPayload) => {
         if (USER_ID != payload.payload.user.id) {
           addMessage(payload.payload);
-          animatedRef.current?.scrollToEnd({ animated: true });
         }
+        animatedRef.current?.scrollToEnd({ animated: true });
       })
       .subscribe();
 
@@ -48,33 +39,9 @@ export default function HomeScreen() {
     };
   }, []);
 
-  const renderItem = ({
-    item,
-    index,
-    separators,
-  }: ListRenderItemInfo<ContentPayload>) => (
-    <Message message={item} index={index} />
-  );
-
-  const renderSeparator = () => (
-    <View
-      style={{
-        height: height * 0.004,
-      }}
-    />
-  );
-
   return (
     <Screen>
-      <Animated.FlatList
-        ref={animatedRef}
-        data={messages}
-        renderItem={renderItem}
-        ItemSeparatorComponent={renderSeparator}
-        keyExtractor={(item, index) => item.content.id}
-        style={[styles.messageList]}
-      />
-
+      <MessageList ref={animatedRef} />
       <BottomChatInput />
     </Screen>
   );
