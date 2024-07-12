1a8e023223680f7847d577908d0985143f9c0b10
Julio Macedo <juliocesar1076@gmail.com>
2024-07-12 13:46:49 -0300

    feat: date messe always white

--- src/components/Message/components/DateMessage.tsx
+++ src/components/Message/components/DateMessage.tsx
@@ -1,7 +1,4 @@
 import { ThemedText } from "@/components/ThemedText";
-import { Colors } from "@/constants/Colors";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { USER_ID } from "@/supabase/supabase";
 import { format } from "date-fns";
 import { StyleProp, StyleSheet, TextStyle } from "react-native";
 type Props = {
@@ -11,14 +8,12 @@ type Props = {
 };
 
 export const DateMessage = ({ date, userId, style }: Props) => {
-  const isUserMessage = USER_ID == userId;
-  const colorScheme = useColorScheme();
   return (
     <ThemedText
       style={[
         styles.messageDate,
         {
-          color: isUserMessage ? "#fff" : Colors[colorScheme ?? "light"].text,
+          color: "#fff",
         },
         style,
       ]}
