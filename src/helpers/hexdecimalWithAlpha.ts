8f959aabd311ebddfea0d4ff8c129b9ea00e9032
Julio Macedo <juliocesar1076@gmail.com>
2024-07-12 13:46:38 -0300

    feat: change type name

--- src/helpers/hexdecimalWithAlpha.ts
+++ src/helpers/hexdecimalWithAlpha.ts
@@ -1,16 +1,19 @@
-type params = {
+type Params = {
   hex: string;
   alpha: number;
 };
 /**
  * Converts a hexadecimal color code to a hexadecimal color code with alpha transparency.
  *
- * @param {Object} params - The parameters for the function.
- * @param {string} params.hex - The hexadecimal color code (e.g., "#FF5733").
- * @param {number} params.alpha - The alpha value from 0 to 1 (e.g., 0.5).
+ * @param {Params} Params - The parameters for the function.
+ * @param {string} Params.hex - The hexadecimal color code (e.g., "#FF5733").
+ * @param {number} Params.alpha - The alpha value from 0 to 1 (e.g., 0.5).
  * @returns {string} The hexadecimal color code with alpha (e.g., "#FF573380").
  */
-export const hexdecimalWithAlpha = ({ alpha, hex }: params) => {
+export const hexdecimalWithAlpha = ({ alpha, hex }: Params) => {
+  if (alpha < 0 || alpha > 1) {
+    throw "Alpha must be between 0 and 1";
+  }
   hex = hex.replace("#", "");
 
   if (hex.length === 3) {
