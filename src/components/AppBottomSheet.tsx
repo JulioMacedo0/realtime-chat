711757081e7755140dda001fb1b277a1b52498e8
Julio Macedo <juliocesar1076@gmail.com>
2024-07-16 18:35:39 -0300

    feat: create app bottom sheet

---
+++ src/components/AppBottomSheet.tsx
@@ -0,0 +1,43 @@
+import React, { useCallback, forwardRef, useMemo } from "react";
+import { Text, StyleSheet, View } from "react-native";
+import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
+
+type Props = {};
+
+export const AppBottomSheet = forwardRef<BottomSheetModal, Props>(({}, ref) => {
+  // variables
+  const snapPoints = useMemo(() => ["40%", "70%", "100%"], []);
+
+  // callbacks
+
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
+  // renders
+  return (
+    <BottomSheetModal
+      ref={ref}
+      index={0}
+      snapPoints={snapPoints}
+      onChange={handleSheetChanges}
+    >
+      <BottomSheetView style={styles.contentContainer}>
+        <Text>Awesome 🎉</Text>
+      </BottomSheetView>
+    </BottomSheetModal>
+  );
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    backgroundColor: "grey",
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
