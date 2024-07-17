400bba5843662348db45ed42d6619a23cda9375b
Julio Macedo <juliocesar1076@gmail.com>
2024-07-17 16:57:54 -0300

    feat: add theme colors

--- src/components/AppBottomSheet.tsx
+++ src/components/AppBottomSheet.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, forwardRef, useMemo } from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, useColorScheme } from "react-native";
 import {
   BottomSheetBackdropProps,
   BottomSheetFooterProps,
@@ -8,97 +8,127 @@ import {
   BottomSheetView,
   BottomSheetBackdrop,
   BottomSheetScrollView,
+  BottomSheetHandle,
 } from "@gorhom/bottom-sheet";
 import { BottomSheetButton } from "./BottomSheetButton";
 import { IconApp } from "./IconApp/IconApp";
+import { BottomSheetDefaultHandleProps } from "@gorhom/bottom-sheet/lib/typescript/components/bottomSheetHandle/types";
+import { Colors } from "@/constants/Colors";
 
 type Props = {};
 
 export const AppBottomSheet = forwardRef<BottomSheetModal, Props>(({}, ref) => {
-  // variables
-  const snapPoints = useMemo(() => ["60%", "100%"], []);
+  const colorScheme = useColorScheme();
+
+  const snapPoints = useMemo(() => ["60%", "90%"], []);
 
   const handleSheetChanges = useCallback((index: number) => {
     console.log("handleSheetChanges", index);
   }, []);
 
-  const FooterComponent = useCallback((props: BottomSheetFooterProps) => {
-    return (
-      <BottomSheetFooter {...props}>
-        <BottomSheetScrollView
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          style={{
-            paddingHorizontal: 16,
-            paddingVertical: 6,
-            backgroundColor: "white",
-            flexDirection: "row",
-            gap: 8,
-            borderTopColor: "#ccc",
-            borderTopWidth: 0.8,
-            flex: 1,
-          }}
-        >
-          <BottomSheetButton
-            isSelected
-            Name="Gallery"
-            bgIconColor="#24A1DE"
-            Icon={
-              <IconApp
-                size={30}
-                lib="MaterialIcons"
-                name="insert-photo"
-                color="#fff"
-              />
-            }
-          />
-          <BottomSheetButton
-            isSelected
-            Name="Wallet"
-            bgIconColor="#24A1DE"
-            Icon={
-              <IconApp size={30} lib="Ionicons" name="wallet" color="#fff" />
-            }
-          />
-          <BottomSheetButton
-            isSelected
-            Name="File"
-            bgIconColor="#24A1DE"
-            Icon={
-              <IconApp
-                size={30}
-                lib="MaterialCommunityIcons"
-                name="file"
-                color="#fff"
-              />
-            }
-          />
-          <BottomSheetButton
-            isSelected
-            Name="Location"
-            bgIconColor="#50C878"
-            Icon={
-              <IconApp
-                size={30}
-                lib="MaterialIcons"
-                name="location-on"
-                color="#fff"
-              />
-            }
-          />
-          <BottomSheetButton
-            isSelected
-            Name="Contact"
-            bgIconColor="#FFD700"
-            Icon={
-              <IconApp size={30} lib="FontAwesome" name="user" color="#fff" />
-            }
-          />
-        </BottomSheetScrollView>
-      </BottomSheetFooter>
-    );
-  }, []);
-
+  const FooterComponent = useCallback(
+    ({ ...props }: BottomSheetFooterProps) => {
+      return (
+        <BottomSheetFooter {...props}>
+          <BottomSheetScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            style={{
+              paddingHorizontal: 16,
+              paddingVertical: 6,
+              backgroundColor: Colors[colorScheme ?? "light"].headerBackground,
+              flexDirection: "row",
+              gap: 8,
+              borderTopColor: "#ccc",
+              borderTopWidth: 0.8,
+              flex: 1,
+            }}
+          >
+            <BottomSheetButton
+              isSelected
+              Name="Gallery"
+              bgIconColor="#24A1DE"
+              Icon={
+                <IconApp
+                  size={30}
+                  lib="MaterialIcons"
+                  name="insert-photo"
+                  color="#fff"
+                />
+              }
+            />
+            <BottomSheetButton
+              isSelected
+              Name="Wallet"
+              bgIconColor="#24A1DE"
+              Icon={
+                <IconApp size={30} lib="Ionicons" name="wallet" color="#fff" />
+              }
+            />
+            <BottomSheetButton
+              isSelected
+              Name="File"
+              bgIconColor="#24A1DE"
+              Icon={
+                <IconApp
+                  size={30}
+                  lib="MaterialCommunityIcons"
+                  name="file"
+                  color="#fff"
+                />
+              }
+            />
+            <BottomSheetButton
+              isSelected
+              Name="Location"
+              bgIconColor="#50C878"
+              Icon={
+                <IconApp
+                  size={30}
+                  lib="MaterialIcons"
+                  name="location-on"
+                  color="#fff"
+                />
+              }
+            />
+            <BottomSheetButton
+              isSelected
+              Name="Contact"
+              bgIconColor="#FFD700"
+              Icon={
+                <IconApp size={30} lib="FontAwesome" name="user" color="#fff" />
+              }
+            />
+          </BottomSheetScrollView>
+        </BottomSheetFooter>
+      );
+    },
+    [colorScheme]
+  );
+  const HandleComponent = useCallback(
+    ({ style, indicatorStyle, ...props }: BottomSheetDefaultHandleProps) => {
+      return (
+        <BottomSheetHandle
+          {...props}
+          style={[
+            style,
+            {
+              backgroundColor: Colors[colorScheme ?? "light"].headerBackground,
+              borderTopLeftRadius: 15,
+              borderTopRightRadius: 15,
+            },
+          ]}
+          indicatorStyle={[
+            indicatorStyle,
+            {
+              backgroundColor: Colors[colorScheme ?? "light"].background,
+            },
+          ]}
+        />
+      );
+    },
+    [colorScheme]
+  );
   const BackdropComponent = useCallback(
     ({ style, ...props }: BottomSheetBackdropProps) => {
       return (
@@ -114,12 +144,14 @@ export const AppBottomSheet = forwardRef<BottomSheetModal, Props>(({}, ref) => {
     },
     []
   );
+
   return (
     <BottomSheetModal
       ref={ref}
       index={0}
       snapPoints={snapPoints}
       onChange={handleSheetChanges}
+      handleComponent={HandleComponent}
       backdropComponent={BackdropComponent}
       footerComponent={FooterComponent}
       animationConfigs={{
@@ -127,7 +159,14 @@ export const AppBottomSheet = forwardRef<BottomSheetModal, Props>(({}, ref) => {
         stiffness: 180,
       }}
     >
-      <BottomSheetView style={styles.contentContainer}>
+      <BottomSheetView
+        style={[
+          styles.contentContainer,
+          {
+            backgroundColor: Colors[colorScheme ?? "light"].headerBackground,
+          },
+        ]}
+      >
         <Text>Awesome 🎉</Text>
       </BottomSheetView>
     </BottomSheetModal>
