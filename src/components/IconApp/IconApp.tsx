d1d0ef1d5ae9838edd51956ae998438239794533
Julio Macedo <juliocesar1076@gmail.com>
2024-07-17 12:06:22 -0300

    feat: add libs

--- src/components/IconApp/IconApp.tsx
+++ src/components/IconApp/IconApp.tsx
@@ -6,6 +6,8 @@ import {
   FontAwesome6,
   MaterialCommunityIcons,
   Feather,
+  MaterialIcons,
+  FontAwesome,
 } from "@expo/vector-icons";
 
 import { type IconProps } from "@expo/vector-icons/build/createIconSet";
@@ -32,12 +34,22 @@ type TFeather = {
   lib: "Feather";
 } & IconProps<ComponentProps<typeof Feather>["name"]>;
 
+type TMaterialIcons = {
+  lib: "MaterialIcons";
+} & IconProps<ComponentProps<typeof MaterialIcons>["name"]>;
+
+type TFontAwesome = {
+  lib: "FontAwesome";
+} & IconProps<ComponentProps<typeof FontAwesome>["name"]>;
+
 type Icon =
   | TIoicons
   | TAntDesign
   | TFontAwesome6
   | TMaterialCommunityIcons
-  | TFeather;
+  | TFeather
+  | TMaterialIcons
+  | TFontAwesome;
 
 export function IconApp(props: Icon) {
   if (props.lib == "AntDesign") {
@@ -48,8 +60,12 @@ export function IconApp(props: Icon) {
     return <FontAwesome6 size={22} {...props} />;
   } else if (props.lib == "MaterialCommunityIcons") {
     return <MaterialCommunityIcons size={22} {...props} />;
-  } else if (props.lib) {
+  } else if (props.lib == "Feather") {
     return <Feather size={22} {...props} />;
+  } else if (props.lib == "MaterialIcons") {
+    return <MaterialIcons size={22} {...props} />;
+  } else if (props.lib == "FontAwesome") {
+    return <FontAwesome size={22} {...props} />;
   } else {
     console.error("Choose a valid icon Lib");
     return <Text>?</Text>;
