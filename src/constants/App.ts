08cd9684894cd436528ae4f40aeb9c83abf12c54
Julio Macedo <juliocesar1076@gmail.com>
2024-07-19 15:41:57 -0300

    feat: create app conts

---
+++ src/constants/App.ts
@@ -0,0 +1,14 @@
+import * as MediaLibrary from "expo-media-library";
+
+export const mediaLibraryAsset = {
+  id: "-1",
+  filename: "example_image.jpg",
+  uri: "file:///path/to/example_image.jpg",
+  mediaType: MediaLibrary.MediaType.photo,
+  width: 1920,
+  height: 1080,
+  creationTime: new Date().getTime(),
+  modificationTime: new Date().getTime(),
+  duration: 0,
+  albumId: "example_album_id",
+};
