a5c71833262b096c0e1ba30eefc1719364f8cc7f
Julio Macedo <juliocesar1076@gmail.com>
2024-07-19 16:17:51 -0300

    feat: add colors

--- src/components/NoCameraErrorView.tsx
+++ src/components/NoCameraErrorView.tsx
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from "react-native";
 export const NoCameraErrorView = () => {
   return (
     <View style={styles.container}>
-      <Text>No camera detected</Text>
+      <Text style={styles.text}>No camera detected</Text>
     </View>
   );
 };
@@ -11,5 +11,11 @@ export const NoCameraErrorView = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+  },
+  text: {
+    color: "#fff",
   },
 });
