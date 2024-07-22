d9813395cf31a53f4f1cb2e9ea59f1e075704af0
Julio Macedo <juliocesar1076@gmail.com>
2024-07-22 17:03:39 -0300

    feat: remove portal provider

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -6,72 +6,67 @@ import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-import { PortalProvider } from "@gorhom/portal";
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <PortalProvider>
-        <BottomSheetModalProvider>
-          <Tabs
-            screenOptions={{
-              tabBarStyle: {
-                backgroundColor:
-                  Colors[colorScheme ?? "light"].tabBarBackground,
-              },
-              headerStyle: {
-                backgroundColor:
-                  Colors[colorScheme ?? "light"].headerBackground,
-              },
-              tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-              headerShown: true,
+      <BottomSheetModalProvider>
+        <Tabs
+          screenOptions={{
+            tabBarStyle: {
+              backgroundColor: Colors[colorScheme ?? "light"].tabBarBackground,
+            },
+            headerStyle: {
+              backgroundColor: Colors[colorScheme ?? "light"].headerBackground,
+            },
+            tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+            headerShown: true,
+          }}
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "Real time chat",
+              headerTitleAlign: "center",
+              tabBarIcon: ({ color, focused }) => (
+                <IconApp
+                  lib="Ionicons"
+                  name={focused ? "home" : "home-outline"}
+                  color={color}
+                />
+              ),
             }}
-          >
-            <Tabs.Screen
-              name="index"
-              options={{
-                title: "Real time chat",
-                headerTitleAlign: "center",
-                tabBarIcon: ({ color, focused }) => (
-                  <IconApp
-                    lib="Ionicons"
-                    name={focused ? "home" : "home-outline"}
-                    color={color}
-                  />
-                ),
-              }}
-            />
-            <Tabs.Screen
-              name="explore"
-              options={{
-                title: "Explore",
-                tabBarIcon: ({ color, focused }) => (
-                  <IconApp
-                    lib="Ionicons"
-                    name={focused ? "code-slash" : "code-slash-outline"}
-                    color={color}
-                  />
-                ),
-              }}
-            />
-            <Tabs.Screen
-              name="todo"
-              options={{
-                title: "todo",
-                headerTitleAlign: "center",
-                tabBarIcon: ({ color, focused }) => (
-                  <IconApp
-                    lib="Ionicons"
-                    name={focused ? "book" : "book-outline"}
-                    color={color}
-                  />
-                ),
-              }}
-            />
-          </Tabs>
-        </BottomSheetModalProvider>
-      </PortalProvider>
+          />
+          <Tabs.Screen
+            name="explore"
+            options={{
+              title: "Explore",
+              tabBarIcon: ({ color, focused }) => (
+                <IconApp
+                  lib="Ionicons"
+                  name={focused ? "code-slash" : "code-slash-outline"}
+                  color={color}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="todo"
+            options={{
+              title: "todo",
+              headerTitleAlign: "center",
+              tabBarIcon: ({ color, focused }) => (
+                <IconApp
+                  lib="Ionicons"
+                  name={focused ? "book" : "book-outline"}
+                  color={color}
+                />
+              ),
+            }}
+          />
+        </Tabs>
+      </BottomSheetModalProvider>
     </GestureHandlerRootView>
   );
 }
