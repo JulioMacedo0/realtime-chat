44c845175866f65e41708e5075d103058c539862
Julio Macedo <juliocesar1076@gmail.com>
2024-07-22 10:57:41 -0300

    feat:  create animated ScrollView

--- src/constants/AnimatedComponents.ts
+++ src/constants/AnimatedComponents.ts
@@ -1,9 +1,10 @@
 import { IconApp } from "@/components";
 import { Pressable, ActivityIndicator, TouchableOpacity } from "react-native";
 import Animated from "react-native-reanimated";
-
+import { ScrollView } from "react-native-gesture-handler";
 export const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 export const AnimatedActivityIndicator =
   Animated.createAnimatedComponent(ActivityIndicator);
 export const AnimatedTouchableOpacity =
   Animated.createAnimatedComponent(TouchableOpacity);
+export const AnimatedScrollView = Animated.createAnimatedComponent(ScrollView);
